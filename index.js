ab6e51f6f9766ff70b02928470d2bf32e94c0f7a
Christopher Forti <christopher.forti.79@gmail.com>
2024-09-04 19:08:59 -0400

    Added app.get, and app.post for username entry.

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/username", function (req, res) {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+  res.json({ usernamename: `${firstName} ${lastName}` });
+});
+
+app.post("/api/users", function (req, res) {
+  const firstName = req.body.first;
+  const lastName = req.body.last;
+  res.json({
+    username: {
+      username: "fcc_test",
+      _id: "5fb5853f734231456ccb3b05",
+    },
+  });
+});
+
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
